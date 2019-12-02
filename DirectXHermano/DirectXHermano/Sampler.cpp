5fc96c0d0688efeecff7f6c8ce3457f5cd85edef
HeladodePistacho <th_sola@hotmail.com>
2019-12-02 15:04:16 +0100

    Added Anisotropic filtering to avoid Blurry angles

--- DirectXHermano/DirectXHermano/Sampler.cpp
+++ DirectXHermano/DirectXHermano/Sampler.cpp
@@ -4,10 +4,11 @@ Sampler::Sampler(Render & ren)
 {
 	D3D11_SAMPLER_DESC sampler_descriptor = {};
 
-	sampler_descriptor.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+	sampler_descriptor.Filter = D3D11_FILTER_ANISOTROPIC;
 	sampler_descriptor.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
 	sampler_descriptor.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
 	sampler_descriptor.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
+	sampler_descriptor.MaxAnisotropy = D3D11_MAX_MAXANISOTROPY;
 
 	sampler_descriptor.MipLODBias = 0.0f;
 	sampler_descriptor.MinLOD = 0.0f;
